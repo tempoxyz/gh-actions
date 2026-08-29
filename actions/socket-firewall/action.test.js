543ddef7204f8f3df8781b41217344d691f1eae9
Shane da Silva <shane@tempo.xyz>
2026-08-28 22:19:50 -0700

    chore: pin masked Socket STS action

--- actions/socket-firewall/action.test.js
+++ actions/socket-firewall/action.test.js
@@ -8,7 +8,7 @@ const manifest = fs.readFileSync(path.join(__dirname, "action.yml"), "utf8");
 test("composes the migrated Socket STS and pinned Firewall actions", () => {
   assert.match(
     manifest,
-    /tempoxyz\/gh-actions\/actions\/socket-firewall\/sts@18832acecd3af6bd2ab87500fa888c664637077c/,
+    /tempoxyz\/gh-actions\/actions\/socket-firewall\/sts@09c541105b7d4e1b23343560e2438f48d874d3bb/,
   );
   assert.match(
     manifest,
