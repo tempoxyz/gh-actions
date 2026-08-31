2140e769770234a7be5780a19f1365b88c3cf427
Shane da Silva <shane@tempo.xyz>
2026-08-30 19:18:50 -0700

    ci: repin Socket STS action to merged revision

--- actions/socket-firewall/action.test.js
+++ actions/socket-firewall/action.test.js
@@ -8,7 +8,7 @@ const manifest = fs.readFileSync(path.join(__dirname, "action.yml"), "utf8");
 test("composes the migrated Socket STS and pinned Firewall actions", () => {
   assert.match(
     manifest,
-    /tempoxyz\/gh-actions\/actions\/socket-firewall\/sts@09c541105b7d4e1b23343560e2438f48d874d3bb/,
+    /tempoxyz\/gh-actions\/actions\/socket-firewall\/sts@cb5d330aa987dbf3e315e3e02f640616ce4e8765/,
   );
   assert.match(
     manifest,
